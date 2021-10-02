ca8f8ae84f6ab358bef10115e41a28e3a70b0241
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-02 14:09:19 -0300

    module basic

--- 01-terraform_lab_exemplos/19-module/1-basico/main.tf
+++ 01-terraform_lab_exemplos/19-module/1-basico/main.tf
@@ -1,64 +1,9 @@
-terraform {
-  required_version = "0.14.4"
+module "objects" {
+  source = "./s3_object"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.23.0"
-    }
-  }
-}
+  for_each = var.files != "" ? fileset(var.files, "**") : []
 
-provider "aws" {
-  region  = "eu-central-1"
-  profile = "tf014"
-}
-
-resource "random_pet" "this" {
-  length = 5
-}
-
-module "bucket" {
-  source = "./s3_module"
-  name   = random_pet.this.id
-
-  versioning = {
-    enabled = true
-  }
-}
-
-resource "random_pet" "website" {
-  length = 5
-}
-
-module "website" {
-  source = "./s3_module"
-
-  name  = random_pet.website.id
-  acl   = "public-read"
-  files = "${path.root}/website"
-
-  website = {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
-  policy = <<EOT
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${random_pet.website.id}/*"
-            ]
-        }
-    ]
-}
-EOT
+  bucket = aws_s3_bucket.this.bucket
+  key    = "${var.key_prefix}/${each.value}"
+  src    = "${var.files}/${each.value}"
 }
--- 01-terraform_lab_exemplos/19-module/1-basico/outputs.tf
+++ 01-terraform_lab_exemplos/19-module/1-basico/outputs.tf
@@ -1,23 +1,3 @@
-output "bucket-name" {
-  value = module.bucket.name
-}
-
-output "bucket-arn" {
-  value = module.bucket.arn
-}
-
-output "bucket-website-name" {
-  value = module.website.name
-}
-
-output "bucket-website-url" {
-  value = module.website.website
-}
-
-output "bucket-website-arn" {
-  value = module.website.arn
-}
-
-output "bucket-website-files" {
-  value = module.website.files
+output "files" {
+  value = [for filename, data in module.objects : filename]
 }
