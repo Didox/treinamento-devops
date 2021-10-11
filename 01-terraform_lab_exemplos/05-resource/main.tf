038cffcd6bc202cd3ebdf240b19d6a077391e37b
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-11 15:41:40 -0300

    mais exemplo

--- 01-terraform_lab_exemplos/05-resource/main.tf
+++ 01-terraform_lab_exemplos/05-resource/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["vpc-321232322"]
+  subnet_id              = "subnet-eddcdzz4"
   # availability_zones = ["us-east-1"] # configurando zona para criar maquinas
   tags = {
     Name = "Minha Maquina Simples EC2"
