b06a594c1468a3627acdfe8a5dc7c242c06d5013
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-15 14:16:27 -0300

    aula ansible turma itaú

--- 02-ansible/terraform/security_group_ssh.tf
+++ 02-ansible/terraform/security_group_ssh.tf
@@ -19,6 +19,17 @@ resource "aws_security_group" "allow_ssh_ansible" {
       prefix_list_ids = null,
       security_groups = null,
       self            = null
+    },
+    {
+      description      = "SSH from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
     }
   ]
 
