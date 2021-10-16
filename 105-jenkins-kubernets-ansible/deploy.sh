920ad5c6238858dcbdd2c27fc92c73591cf979f0
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-15 23:53:04 -0300

    aulas com itau

--- 105-jenkins-kubernets-ansible/deploy.sh
+++ 105-jenkins-kubernets-ansible/deploy.sh
@@ -5,11 +5,14 @@ cd 0-terraform
 ~/terraform/terraform fmt
 ~/terraform/terraform apply -auto-approve
 
+echo "Aguardando criação de maquinas ..."
+sleep 10 # 10 segundos
+
 echo $"[ec2-jenkins]" > ../1-ansible/hosts # cria arquivo
 echo "$(~/terraform/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
-sleep 30 # 30 segundos
+sleep 20 # 20 segundos
 
 cd ../1-ansible
 ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
@@ -18,6 +21,10 @@ cd ../0-terraform
 ~/terraform/terraform output
 
 echo $"Agora somente abrir a URL: http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
+
+# open "http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #mac
+# explorer "http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #windows
+# nautilus "http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #linux
 
 ID_MAQUINA=$(~/terraform/terraform output | grep id | awk '{print $2;exit}')
 echo ${ID_MAQUINA/\",/}
