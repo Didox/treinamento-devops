b94a4be3ecaf5c8886a8b7e6f08d1cca6c1416d5
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-20 09:49:31 -0300

    pegando ultima tag git

--- 06-docker-e-docker-compose/1.2-java-container-env/app-java/build_docker.sh
+++ 06-docker-e-docker-compose/1.2-java-container-env/app-java/build_docker.sh
@@ -1,1 +1,10 @@
-docker build -t didox/crud-java-login:v0.0.1 -f Dockerfile .
+#!/bin/bash -e
+# echo "Digite a tag que deseja para deploy"
+# read tag
+# git checkout tag
+# docker build -t "didox/crud-java-login:$tag" -f Dockerfile .
+
+#### Pegando ultima tag do git #####
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo "Fazendo deploy da tag $tag"
+docker build -t "didox/crud-java-login:$tag" -f Dockerfile .
