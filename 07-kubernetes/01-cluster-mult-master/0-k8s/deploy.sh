0e79839ba60932e32cf1d364daaaa606205bf9af
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-21 12:22:31 -0300

    deploy multimaster shell

--- 07-kubernetes/01-cluster-mult-master/0-k8s/deploy.sh
+++ 07-kubernetes/01-cluster-mult-master/0-k8s/deploy.sh
@@ -123,4 +123,45 @@ ff02::3 ip6-allhosts
 " > ../2-ansible/01-k8s-install-masters_e_workers/host/hosts
 
 cd ../2-ansible/01-k8s-install-masters_e_workers
-ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
+ANSIBLE_OUT=ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
+
+K8S_JOIN_MASTER=$(echo $ANSIBLE_OUT | grep -oP "(kubeadm join.*?certificate-key.*?)'" | sed 's/\\//g' | sed "s/'t//g" | sed "s/'//g" | sed "s/,//g")
+K8S_JOIN_WORKER=$(echo $ANSIBLE_OUT | grep -oP "(kubeadm join.*?discovery-token-ca-cert-hash.*?)'" | head -n 1 | sed 's/\\//g' | sed "s/'t//g" | sed "s/'//g" | sed "s/'//g" | sed "s/,//g")
+
+echo $K8S_JOIN_MASTER
+echo $K8S_JOIN_WORKER
+
+echo "
+- hosts:
+  - ec2-k8s-m2
+  - ec2-k8s-m3
+  become: yes
+  tasks:
+    - name: \"Fazendo join kubernetes master\"
+      shell: $K8S_JOIN_MASTER
+
+    - name: \"Colocando no path da maquina o conf do kubernetes\"
+      shell: mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config && export KUBECONFIG=/etc/kubernetes/admin.conf
+#---
+- hosts:
+  - ec2-k8s-w1
+  - ec2-k8s-w2
+  - ec2-k8s-w3
+  become: yes
+  tasks:
+    - name: \"Fazendo join kubernetes worker\"
+      shell: $K8S_JOIN_WORKER
+
+#---
+- hosts:
+  - ec2-k8s-m1
+  become: yes
+  tasks:
+    - name: \"Configura weavenet para reconhecer os nós master e workers\"
+      shell: kubectl apply -f \"https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')\"
+
+# ansible-playbook -i hosts 2-provisionar-k8s-master.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
+
+" > 2-provisionar-k8s-master-auto-shell.yml
+
+ansible-playbook -i hosts 2-provisionar-k8s-master-auto-shell.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
