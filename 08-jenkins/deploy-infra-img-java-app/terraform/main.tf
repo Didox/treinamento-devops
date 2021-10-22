f3ed377b3e46d853337fad883ae71bf97070c3a4
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-22 12:35:17 -0300

    script jenkins

--- 08-jenkins/deploy-infra-img-java-app/terraform/main.tf
+++ 08-jenkins/deploy-infra-img-java-app/terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "acesso_jenkins_dev_img" {
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0/0"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
