76272605c7c0a8604d0de0941de8384b850f4819
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-22 18:18:48 -0300

    deploya pp

--- 08-jenkins/deploy-dev-java-app/deploy.sh
+++ 08-jenkins/deploy-dev-java-app/deploy.sh
@@ -8,9 +8,9 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-ec2-devs-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(/home/ubuntu/terraform output | grep public_dns-0 | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
-echo "$(/home/ubuntu/terraform output | grep public_dns-1 | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
-echo "$(/home/ubuntu/terraform output | grep public_dns-2 | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(/home/ubuntu/terraform output | grep public_dns-dev | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(/home/ubuntu/terraform output | grep public_dns-stage | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(/home/ubuntu/terraform output | grep public_dns-prod | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
