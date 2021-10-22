7b183a2ba2f8a2a7265feb376fc60769ba74d4b9
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-10-22 16:42:20 -0300

    ami e destroy pipeline

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -5,13 +5,13 @@ cd 08-jenkins/deploy-infra-img-java-app/terraform
 /home/ubuntu/terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
-# sleep 10 # 10 segundos
+sleep 10 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
 echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
-# sleep 10 # 10 segundos
+sleep 10 # 10 segundos
 
 cd ../ansible
 
