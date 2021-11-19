7a39ac57858430c52edc4b41a78c8e01f8fae626
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-11-19 15:39:05 -0300

    cluester multimaster

--- 07-kubernetes/01-cluster-mult-master/0-k8s/deploy.sh
+++ 07-kubernetes/01-cluster-mult-master/0-k8s/deploy.sh
@@ -171,13 +171,13 @@ cat <<EOF > 2-provisionar-k8s-master-auto-shell.yml
     - name: "Configura weavenet para reconhecer os nós master e workers"
       shell: kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=\$(kubectl version | base64 | tr -d '\n')"
 
-  - name: Espera 30 segundos
-    wait_for: timeout=30
+    - name: Espera 30 segundos
+      wait_for: timeout=30
 
-  - shell: kubectl get nodes -o wide
-    register: ps
-  - debug:
-      msg: " '{{ ps.stdout_lines }}' "
+    - shell: kubectl get nodes -o wide
+      register: ps
+    - debug:
+        msg: " '{{ ps.stdout_lines }}' "
 EOF
 
 ansible-playbook -i hosts 2-provisionar-k8s-master-auto-shell.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
