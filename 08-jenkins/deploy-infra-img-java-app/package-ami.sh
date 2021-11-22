72da5fed2e04b5d7d13555b26d453d5cc4a458e4
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-11-22 12:50:23 -0300

    update no curso

--- 08-jenkins/deploy-infra-img-java-app/package-ami.sh
+++ 08-jenkins/deploy-infra-img-java-app/package-ami.sh
@@ -3,8 +3,8 @@
 VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 cd 08-jenkins/deploy-infra-img-java-app/terraform
-RESOURCE_ID=$(/home/ubuntu/terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
+RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
 
 cd ../terraform-ami
-/home/ubuntu/terraform init
-TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID /home/ubuntu/terraform apply -auto-approve
+terraform init
+TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
