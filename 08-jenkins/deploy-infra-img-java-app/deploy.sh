c59661d2b31c4cb06de2eccffaefda1274e461a5
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-12-16 17:20:04 -0300

    pipeline infra

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -15,5 +15,5 @@ sleep 10 # 10 segundos
 
 cd ../ansible
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/chave-privada.pem ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/chave-privada.pem
--- 08-jenkins/deploy-infra-img-java-app/terraform/main.tf
+++ 08-jenkins/deploy-infra-img-java-app/terraform/main.tf
@@ -9,7 +9,7 @@ data "http" "myip" {
 resource "aws_instance" "dev_img_deploy_jenkins" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
-  key_name      = "chave-jenkins"
+  key_name      = "treinamento-turma1_itau"
   tags = {
     Name = "dev_img_deploy_jenkins"
   }
