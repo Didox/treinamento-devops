37e8f8c9cb3077a5146f03a9e5bb76fbcaf64495
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-12-16 17:26:39 -0300

    pipeline infra

--- 08-jenkins/deploy-infra-img-java-app/test.sh
+++ 08-jenkins/deploy-infra-img-java-app/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd 08-jenkins/deploy-infra-img-java-app/terraform
 
-uri=$(/home/ubuntu/terraform output | grep public_ip | awk '{print $2;exit}' | sed -e "s/\",//g")
+uri=$(terraform output | grep public_ip | awk '{print $2;exit}' | sed -e "s/\",//g")
 
 echo $uri
 
