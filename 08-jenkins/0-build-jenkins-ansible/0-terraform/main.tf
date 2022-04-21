4f7ac5d6766b1091f10a13849d60a96c8799dd04
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-04-21 06:15:50 -0300

    ajustes jenkins

--- 08-jenkins/0-build-jenkins-ansible/0-terraform/main.tf
+++ 08-jenkins/0-build-jenkins-ansible/0-terraform/main.tf
@@ -6,14 +6,21 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
 }
 
+resource "aws_key_pair" "chave_ssh_desafio" {
+  key_name   = "chave_ssh_desafio_pub"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+5jxtFhMTOhwTDTaQCkedHuQ3UB6yGMMfubyPRoZQEYHnJAUFOfi2zjkbZqjwT3W0J4mLpWoMP3a89MXhpg/fy2/pyvADinXJFBqLaxzPGVpNCUPGhWuWL4i/6aToAxlg6RZXAeVWW7W3oEjnVzHi98Gw0SveACDttnQKoHPKgpdPNFCxDb3xjUo39qTaV6zmZDh/89C+e3VZw/hLl2YrFKvBLqs2efZmiqQP8sPHfIQA01YTVc+6zykNIc+X0FtAzf1Wsw0BenHBdKTFR1O/PKgQuEGQ+ez0B2h6aG17UHowY8uDCfNN/mtZGN8be6MAUYudKT43a7utDFWzNPCf3N0YbvrZsTGkqaeWMKw4SGvciIWJstimR61/jEAb+uBNGPdfEzIwNcZxfSBEIj1HCo+Y1a7VyeyR+cEvAbxpVifNmOvI20090vC3o1TpK2zU0zzbGZWBBlBN3f2qWrUB0nEKlBx1iijAiHVG7AhocuBji+/IoMOKvtjWhYAWKtM= danilo@Macbooks-MacBook-Pro.local"
+}
+
 resource "aws_instance" "jenkins" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.large"
-  key_name      = "treinamento-turma1"
+  key_name      = aws_key_pair.chave_ssh_desafio.key_name
   tags = {
     Name = "jenkins"
   }
-  vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
+  vpc_security_group_ids = [
+    aws_security_group.jenkins.id
+  ]
 }
 
 resource "aws_security_group" "jenkins" {
@@ -72,6 +79,6 @@ output "jenkins" {
     "private: ${aws_instance.jenkins.private_ip}",
     "public: ${aws_instance.jenkins.public_ip}",
     "public_dns: ${aws_instance.jenkins.public_dns}",
-    "ssh -i ~/Desktop/devops/Treinamento ubuntu@${aws_instance.jenkins.public_dns}"
+    "ssh -i /Users/danilo/Desktop/desafio_devops/terraform/ssh/id_rsa ubuntu@${aws_instance.jenkins.public_dns}"
   ]
 }
--- 08-jenkins/0-build-jenkins-ansible/deploy.sh
+++ 08-jenkins/0-build-jenkins-ansible/deploy.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 cd 0-terraform
-~/terraform/terraform init
-~/terraform/terraform fmt
-~/terraform/terraform apply -auto-approve
+terraform init
+terraform fmt
+terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo "[ec2-jenkins]" > ../1-ansible/hosts # cria arquivo
-echo "$(~/terraform/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 20 # 20 segundos
 
 cd ../1-ansible
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/Desktop/devops/Treinamento
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /Users/danilo/Desktop/desafio_devops/terraform/ssh/id_rsa
 
 cd ../0-terraform
-~/terraform/terraform output
+terraform output
 
-echo "Agora somente abrir a URL: http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
+echo "Agora somente abrir a URL: http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
 
-# open "http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #mac
-# explorer "http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #windows
-# nautilus "http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #linux
+# open "http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #mac
+# explorer "http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #windows
+# nautilus "http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #linux
 
-ID_MAQUINA=$(~/terraform/terraform output | grep id | awk '{print $2;exit}')
+ID_MAQUINA=$(terraform output | grep id | awk '{print $2;exit}')
 echo ${ID_MAQUINA/\",/}
 
 # cd ../2-terraform-ami/
 
-# ~/terraform/terraform init
-# ~/terraform/terraform fmt
-# ~/terraform/terraform apply -var="resource_id=${ID_MAQUINA/\",/}" -auto-approve 
-# # ~/terraform/terraform apply -var="resource_id=${ID_MAQUINA/\",/} versao=0.0.2" -auto-approve 
-# ~/terraform/terraform output
+# terraform init
+# terraform fmt
+# terraform apply -var="resource_id=${ID_MAQUINA/\",/}" -auto-approve 
+# # terraform apply -var="resource_id=${ID_MAQUINA/\",/} versao=0.0.2" -auto-approve 
+# terraform output
