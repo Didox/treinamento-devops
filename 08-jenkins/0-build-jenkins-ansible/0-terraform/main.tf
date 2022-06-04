f51445c65cbfbc817401c69594f3e15df77524d0
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-06-04 08:29:11 -0300

    deploy azure k8s

--- 08-jenkins/0-build-jenkins-ansible/0-terraform/main.tf
+++ 08-jenkins/0-build-jenkins-ansible/0-terraform/main.tf
@@ -6,15 +6,15 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
 }
 
-resource "aws_key_pair" "chave_ssh_desafio" {
-  key_name   = "chave_ssh_desafio_pub"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+5jxtFhMTOhwTDTaQCkedHuQ3UB6yGMMfubyPRoZQEYHnJAUFOfi2zjkbZqjwT3W0J4mLpWoMP3a89MXhpg/fy2/pyvADinXJFBqLaxzPGVpNCUPGhWuWL4i/6aToAxlg6RZXAeVWW7W3oEjnVzHi98Gw0SveACDttnQKoHPKgpdPNFCxDb3xjUo39qTaV6zmZDh/89C+e3VZw/hLl2YrFKvBLqs2efZmiqQP8sPHfIQA01YTVc+6zykNIc+X0FtAzf1Wsw0BenHBdKTFR1O/PKgQuEGQ+ez0B2h6aG17UHowY8uDCfNN/mtZGN8be6MAUYudKT43a7utDFWzNPCf3N0YbvrZsTGkqaeWMKw4SGvciIWJstimR61/jEAb+uBNGPdfEzIwNcZxfSBEIj1HCo+Y1a7VyeyR+cEvAbxpVifNmOvI20090vC3o1TpK2zU0zzbGZWBBlBN3f2qWrUB0nEKlBx1iijAiHVG7AhocuBji+/IoMOKvtjWhYAWKtM= danilo@Macbooks-MacBook-Pro.local"
-}
+# resource "aws_key_pair" "chave_ssh_desafio" {
+#   key_name   = "chave_ssh_desafio_pub"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6n+egdpEqWRcnPIc0D/c7IVgErojxHldA8C1iFKIKX98GlSXMmzVdVedxMjcs6AMU0puGGBQruQEy9HqqwsbFcEyjPofIJa4oztyXn0VqSPjdWsD3dr/5IkUu38Q0CooLgOXfabSIytwj+810cWX7Kno2GVAfOLKRI0/+JE9Q4poX47DpyF8yN3dRN6aitQmnPioq3NwViHbdHpe7mGhUju6nItDNuN39KeCgA4s2yuDz42sFXsBxjdt3F7j6iaq3iPpOC8+5sYpER1KpxQ4JO1g3dsIOoNzlyLvkUMY46JKCoRh/0crTRKhNXhARiFUpNWOQDfyLFepSD9+hq9Xfa6es4rXKGi0eSv9v5Fc119hSVObjoF7760FDSp/1IMccQw06JS2V/HRHIY9+s5Pm9ChoO/tpdglxPszD2vWjE7JrvHXVZlxyTEi5SQOTBGM/s3dONaW0/tU9ZMgNOgMZ1AOkspyDltXsP2hDK6fOLj5rt8P1vqq//D2wY1Pc04M= danilo@Macbooks-MacBook-Pro.local"
+# }
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-09e67e426f25ce0d7"
+  ami           = "ami-09d56f8956ab235b3"
   instance_type = "t2.large"
-  key_name      = aws_key_pair.chave_ssh_desafio.key_name
+  key_name      = "chave_publica_desafio_kubernetes"
   tags = {
     Name = "jenkins"
   }
@@ -33,7 +33,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -44,7 +44,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 8080
       to_port          = 8080
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -79,6 +79,6 @@ output "jenkins" {
     "private: ${aws_instance.jenkins.private_ip}",
     "public: ${aws_instance.jenkins.public_ip}",
     "public_dns: ${aws_instance.jenkins.public_dns}",
-    "ssh -i /Users/danilo/Desktop/desafio_devops/terraform/ssh/id_rsa ubuntu@${aws_instance.jenkins.public_dns}"
+    "ssh -i ~/Desktop/devops/id_rsa ubuntu@${aws_instance.jenkins.public_dns}"
   ]
 }
--- 08-jenkins/0-build-jenkins-ansible/deploy.sh
+++ 08-jenkins/0-build-jenkins-ansible/deploy.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 cd 0-terraform
-terraform init
-terraform fmt
-terraform apply -auto-approve
+/Users/Danilo/terraform/terraform init
+/Users/Danilo/terraform/terraform fmt
+/Users/Danilo/terraform/terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo "[ec2-jenkins]" > ../1-ansible/hosts # cria arquivo
-echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(/Users/Danilo/terraform/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 20 # 20 segundos
 
 cd ../1-ansible
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /Users/danilo/Desktop/desafio_devops/terraform/ssh/id_rsa
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /Users/Danilo/Desktop/devops/id_rsa
 
 cd ../0-terraform
-terraform output
+/Users/Danilo/terraform/terraform output
 
-echo "Agora somente abrir a URL: http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
+echo "Agora somente abrir a URL: http://$(/Users/Danilo/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
 
 # open "http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #mac
 # explorer "http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #windows
 # nautilus "http://$(terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"" #linux
 
-ID_MAQUINA=$(terraform output | grep id | awk '{print $2;exit}')
+ID_MAQUINA=$(/Users/Danilo/terraform/terraform output | grep id | awk '{print $2;exit}')
 echo ${ID_MAQUINA/\",/}
 
 # cd ../2-terraform-ami/
